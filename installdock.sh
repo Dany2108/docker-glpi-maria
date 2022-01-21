0f9b076333fe773075200c6cc8f3a0a9ca5c255d
Dany2108 <dvincent.freelance@gmail.com>
2022-01-21 13:21:18 +0100

    docker-glpi-maria

--- installdock.sh
+++ installdock.sh
@@ -13,3 +13,8 @@ echo \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install docker-compose
+
+sudo usermod -aG docker $USER
+sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo service docker restart
